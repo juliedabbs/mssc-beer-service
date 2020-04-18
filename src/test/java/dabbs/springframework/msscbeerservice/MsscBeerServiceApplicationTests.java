af77dd66ec016549d8f3c05a044d752b87b61684
Julie Dabbs <jzd@phoebussoftware.com>
2020-04-18 15:17:36 +0100

    Initial Project Creation

---
+++ src/test/java/dabbs/springframework/msscbeerservice/MsscBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dabbs.springframework.msscbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBeerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
